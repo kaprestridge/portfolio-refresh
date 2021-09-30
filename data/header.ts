6c001fd4145b3307d705aa994b47c9f7e2672f5c
kaprestridge <kaprestridge@gmail.com>
2021-09-30 11:07:52 +1000

    added data

---
+++ data/header.ts
@@ -0,0 +1,22 @@
+export const headerData = [
+	{
+		id: 0,
+		title: "TWITTER",
+		link: "twitter.com"
+	},
+	{
+		id: 1,
+		title: "GITHUB",
+		link: "github.com"
+	},
+	{
+		id: 2,
+		title: "LINKEDIN",
+		link: "linkedin.com"
+	},
+	{
+		id: 3,
+		title: "RESUME",
+		link: "#"
+	},
+]
