e345d9705b301e4f929c1c9966dd340da2089c2a
kaprestridge <kaprestridge@gmail.com>
2021-10-01 10:12:33 +1000

    added placeholder image to hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -88,7 +88,14 @@ export const Hero: FunctionComponent = () => {
             </div>
           </div>
 
-          <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
+          <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 flex flex-column">
+            <div className="md:place-content-center">
+              <img
+                className="object-fill w-full h-48"
+                src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg"
+                alt="Placeholderimage"
+              />
+            </div>
             <div className="">
               <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
                 {"DEVELOPER"}
