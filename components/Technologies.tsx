04ec5d963b3a964517dd7f12f038edb275dbb8e6
kaprestridge <kaprestridge@gmail.com>
2021-10-05 14:46:53 +1100

    added alt text and key

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -2,13 +2,18 @@ import Image from "next/dist/client/image";
 import { FunctionComponent } from "react";
 
 export interface TechnologyProps {
-  data: { id: number; title: string; image: string; blurb: string }[];
+  data: {
+    id: number;
+    title: string;
+    image: string;
+    alt: string;
+    blurb: string;
+  }[];
 }
 
 export const Technologies: FunctionComponent<TechnologyProps> = ({ data }) => {
   return (
     <>
-      {console.log(data)}
       <h1>
         <span className="m-5 block  text-center tracking-wide text-5xl font-extrabold text-indigo-700">
           {"Technical skill set"}
@@ -16,10 +21,13 @@ export const Technologies: FunctionComponent<TechnologyProps> = ({ data }) => {
       </h1>
       <div className="mx-10 md:mx-20 rounded-lg bg-gray-200 overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px">
         {data.map(item => (
-          <div className="  sm:rounded-tr-none relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+          <div
+            key={item.id}
+            className="  sm:rounded-tr-none relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500"
+          >
             <div>
               <span className="rounded-lg inline-flex p-3 bg-indigo-50 ring-4 ring-white">
-                <Image src={item.image} width={20} height={20} />
+                <Image src={item.image} width={20} height={20} alt={item.alt} />
               </span>
             </div>
             <div className="mt-8">
