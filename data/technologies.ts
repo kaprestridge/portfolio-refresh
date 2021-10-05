af85a1ea2a331130d03b883e71829ebcb830a21a
kaprestridge <kaprestridge@gmail.com>
2021-10-05 14:38:55 +1100

    added data and svgs for technoly section, imported data in index

---
+++ data/technologies.ts
@@ -0,0 +1,28 @@
+export const technologyData = [
+  {
+    id: 0,
+    title: "Typescript / React / NextJS",
+    image: "/../public/images/typescript.svg",
+    blurb: "Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit viverra aliquam porttitor ultrices gravida eu."
+  },
+  {
+    id: 1,
+    title: "Ruby / Rails",
+    image: "/../public/images/ruby.svg",
+    blurb: "Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit viverra aliquam porttitor ultrices gravida eu."
+  },
+  {
+    id: 2,
+    title: "Elixir",
+    image: "/../public/images/elixir.svg",
+    blurb: "Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit viverra aliquam porttitor ultrices gravida eu."
+  },
+
+  {
+    id: 3,
+    title: "HTML / CSS / Tailwind",
+    image: "/../public/images/html.svg",
+    blurb: "Quis semper vulputate aliquam venenatis egestas sagittis quisque orci. Donec commodo sit viverra aliquam porttitor ultrices gravida eu."
+  },
+
+]
