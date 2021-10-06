77ce23ee3afbac5998e22f1b456c07cd9483a1fe
kaprestridge <kaprestridge@gmail.com>
2021-10-07 10:52:40 +1100

    added hamburger menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,7 @@
 import React, { FunctionComponent } from "react";
-// import Image from 'next/image'
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 export interface HeaderProps {
   data: { id: number; title: string; link: string }[];
@@ -7,45 +9,89 @@ export interface HeaderProps {
 
 export const Header: FunctionComponent<HeaderProps> = ({ data }) => {
   return (
-    <>
-      <header className="bg-white">
+    <Popover>
+      <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav
-          className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"
-          aria-label="Top"
+          className="relative flex items-center justify-between h-20"
+          aria-label="Global"
         >
-          <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
-            <div className="flex items-center">
-              <div className="hidden ml-10 space-x-8 lg:block">
-                <a
-                  href="#"
-                  className="text-base font-medium text-indigo-700 hover:text-indigo-900"
-                  key="Solutions"
-                >
-                  <span className="text-base font-extrabold text-indigo-700">
-                    KATHRYN
-                  </span>
-                  <span className="text-base font-extrabold text-indigo-900">
-                    PRESTRIDGE
-                  </span>
-                </a>
+          <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+            <div className="flex items-center justify-between w-full md:w-auto">
+              <a href="#">
+                <span className="sr-only">Workflow</span>
+                <span className="text-base font-extrabold text-indigo-700">
+                  KATHRYN
+                </span>
+                <span className="text-base font-extrabold text-indigo-900">
+                  PRESTRIDGE
+                </span>
+              </a>
+              <div className="-mr-2 flex items-center md:hidden">
+                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Open main menu</span>
+                  <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                </Popover.Button>
               </div>
             </div>
-            <div className="ml-10 space-x-6">
+          </div>
+          <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8 float-left">
+            {data.map(item => (
+              <a
+                key={item.title}
+                href={item.link}
+                className="font-medium text-indigo-600 hover:text-indigo-900"
+              >
+                {item.title}
+              </a>
+            ))}
+          </div>
+        </nav>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="duration-150 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-100 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Popover.Panel
+          focus
+          className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+        >
+          <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+            <div className="px-5 pt-4 flex items-center justify-between">
+              <div>
+                <span className="text-base font-extrabold text-indigo-700">
+                  KATHRYN
+                </span>
+                <span className="text-base font-extrabold text-indigo-900">
+                  PRESTRIDGE
+                </span>
+              </div>
+              <div className="-mr-2">
+                <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Close main menu</span>
+                  <XIcon className="h-6 w-6" aria-hidden="true" />
+                </Popover.Button>
+              </div>
+            </div>
+            <div className="px-2 pt-2 pb-3 space-y-1">
               {data.map(item => (
                 <a
+                  key={item.title}
                   href={item.link}
-                  className="text-base font-medium text-indigo-700 hover:text-indigo-900"
-                  key={item.id}
-                  target="_blank"
-                  rel="noreferrer"
+                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
                 >
                   {item.title}
                 </a>
               ))}
             </div>
           </div>
-        </nav>
-      </header>
-    </>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
   );
 };
